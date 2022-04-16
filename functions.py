8fe2ff018cd58a16b644b0bf6b3179f59d5c4f2f
Sriya Balineni <33444116+sriyabalineni@users.noreply.github.com>
2022-04-15 17:39:09 -0700

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,3 +1,61 @@
+import pymongo
+from pymongo import MongoClient
+
+menu_option = {1: 'Find Business by Name', 2: 'Find Nearby Businesses', 3: 'Find Businesses by Category.',
+               4: 'Find High Rated Businesses', 5: 'Find Popular Businesses', 6: 'Exit'}
+
+
+def print_menu(menu_option):
+    for key, value in menu_option.items():
+        print(key, ".", value)
+
+
+def main():
+    mongoClient = None
+    try:
+        #mongoClient = MongoClient("localhost:27020")
+        mongoClient = MongoClient()
+        print("Connected successfully!!!")
+    except:
+        print("Could not connect to MongoDB")
+
+    db = mongoClient.yelpdb
+    collection = db.yelpc
+
+    while (True):
+        print_menu(menu_option)
+        option = int(input('Enter your choice: '))
+
+        if option == 6:
+            mongoClient.close()
+            break
+        elif option==1:
+            find_business_by_name(collection)
+            break
+        elif option==2:
+            break
+        elif option==3:
+            find_businesses_by_category(collection)
+            break
+        elif option==4:
+            fetch_high_rated_business(collection)
+            break
+        elif option==5:
+            fetch_popular_business(collection)
+            break
+
+
+# function 1
+def find_business_by_name(collection):
+    name = input("Please Enter Business Name: ")
+    output = collection.find({"name": {"$regex": name}},
+                             {"name": 1, "address": 1, "city": 1, "state": 1, "postal_code": 1,
+                              "stars": 1, "review_count": 1}).limit(5)
+    for doc in output:
+        print(doc)
+
+
+# function 2 PETER
 # function 3
 def find_businesses_by_category(collection):
     category = input("Please enter a category!\nFor example: Fast Food, Sushi, Italian, etc: ")
@@ -37,4 +95,4 @@ def fetch_popular_business(collection):
 
 if __name__=="__main__":
     main()
-                                                                                                                 99,10         Bot
+    
